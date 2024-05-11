2683e96922dfe6d27f28f7c4e52df9c7147a5e02
aryan <aryanrdat@gmail.com>
2024-05-11 10:14:36 +0530

    UI of Multiple user meeting added

---
+++ lib/models/user.dart
@@ -0,0 +1,15 @@
+class UserModel
+{
+  String id;
+  String name;
+  String photo;
+  String email;
+
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.photo,
+    required this.email,
+  });
+}
